19b939ae9cd0584304d237a60d5a484d48347703
moshonk <omondi.ken@gmail.com>
2023-06-22 15:15:31 +0300

    Added basic hibernate exercise example

--- EH203 Enterprise Java/Public/Exercises/08 SQL-JDBC/JDBC/src/Main.java
+++ EH203 Enterprise Java/Public/Exercises/08 SQL-JDBC/JDBC/src/Main.java
@@ -1,16 +1,27 @@
 import domain.Project;
+import hibernateemr.HibernateUtil;
 import manager.ProjectManager;
+import org.hibernate.Session;
 
 public class Main {
 	public static void main(String[] args) {
-		ProjectManager projectManager = new ProjectManager();
+	//	ProjectManager projectManager = new ProjectManager();
 	//	Project project = new Project();
 	//	project.setProjectName("ICAP OpenMRS Training 1");
 	//	projectManager.addProject(project);
 		
-		for (Project project : projectManager.getProjectList()) {
-			System.out.println(project.getProjectName());
-		}
+	//	for (Project project : projectManager.getProjectList()) {
+	//		System.out.println(project.getProjectName());
+	//	}
+		
+		// HibernateUtil.getSessionFactory().getCurrentSession();
+		
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Project project = (Project) session.load(Project.class, 1); // Loads project record with ID=1
+		session.getTransaction().commit();
+		
+		System.out.println("Project name is  " + project.getProjectName());
 		
 	}
 	
